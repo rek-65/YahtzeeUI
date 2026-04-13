c37cbde91bf54dac3537317d74e369c31d2b3748
rek-65 <rek@gorok.biz>
2026-04-13 16:39:07 -0500

    Remove local JARs and switch to JitPack dependency for engine

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,14 @@ dependencyResolutionManagement {
 
 rootProject.name = "YahtzeeUI"
 include(":app")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
  
